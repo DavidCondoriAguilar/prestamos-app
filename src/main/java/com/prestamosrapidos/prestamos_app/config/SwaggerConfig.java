90da1c28226da976220105a6457b4e170dee7472
David <davidragewild@gmail.com>
2025-01-18 11:13:55 -0500

    add: swagerUI

---
+++ src/main/java/com/prestamosrapidos/prestamos_app/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.prestamosrapidos.prestamos_app.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Préstamos Rápidos")
+                        .version("1.0")
+                        .description("Documentación interactiva de la API de Préstamos Rápidos")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
